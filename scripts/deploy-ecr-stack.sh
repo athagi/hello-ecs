b2737c207a3984b91ae7a227ca1074c4e2b02925
athagi <at.hgt.dev@gmail.com>
2021-10-13 10:32:12 +0900

    add ecr stack

---
+++ scripts/deploy-ecr-stack.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+STACK_NAME=${1:-ecr-test-stack}
+REPO_NAME=${2:-test}
+
+aws cloudformation deploy --template-file ecr.yaml --stack-name "${STACK_NAME}" --parameter-override ECRRepoName=${REPO_NAME}
