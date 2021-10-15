afc3cc8f331700b34e63dd4bc1a6a9cc35735371
athagi <at.hgt.dev@gmail.com>
2021-10-15 10:29:54 +0900

    use rain to deploy stack

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hello-ecs
+
+# installation
+
+[rain](https://github.com/aws-cloudformation/rain) >= v1.2.0
--- scripts/deploy-ecr-stack.sh
+++ scripts/deploy-ecr-stack.sh
@@ -2,4 +2,6 @@
 STACK_NAME=${1:-ecr-test-stack}
 REPO_NAME=${2:-test}
 
-aws cloudformation deploy --template-file ecr.yaml --stack-name "${STACK_NAME}" --parameter-override ECRRepoName="${REPO_NAME}"
+cd `dirname $0`
+
+rain deploy ../ecr.yaml --params ECRRepoName="${REPO_NAME}" "${STACK_NAME}" -y
