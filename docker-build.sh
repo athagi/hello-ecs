a6b901f4e03c00357c4df085e17049f8e0f49c28
athagi <at.hgt.dev@gmail.com>
2021-10-18 09:57:44 +0900

    add docker-build in ci

--- docker-build.sh
+++ docker-build.sh
@@ -2,10 +2,13 @@
 COMMIT_HASH=${1:-none}
 IS_DEVEL=${2:-true} # devel or latest
 TAG="latest"
+APP_NAME="my-app"
+
+cd "$(dirname "$0")" || exit
 
 if [ "${IS_DEVEL}" = "true" ]; then
   TAG="devel"
 fi
 
-DOCKER_BUILDKIT=1 docker build --tag "${COMMIT_HASH}" --tag "${TAG}" .
+DOCKER_BUILDKIT=1 docker build . --file Dockerfile --tag "${APP_NAME}":"${COMMIT_HASH}" --tag "${APP_NAME}":"${TAG}"
 
